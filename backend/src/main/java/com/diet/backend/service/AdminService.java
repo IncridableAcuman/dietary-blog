63bb7367b1ab954a42965ebb80a78e5595178bf9
Izzatbek <abdusharipovizzat03@gmail.com>
2025-12-06 13:27:09 +0500

    redis configuration file ready

---
+++ backend/src/main/java/com/diet/backend/service/AdminService.java
@@ -0,0 +1,13 @@
+package com.diet.backend.service;
+
+import com.diet.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+    private final UserRepository userRepository;
+
+
+}
